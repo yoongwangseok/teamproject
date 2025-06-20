30d7d1745b0b7df025b4064926e616d8101ecc57
yoongwangseok <bartender.pello@gmail.com>
2025-06-20 19:26:27 +0900

    기본css만 완성..

--- src/pages/Profile/ProfilePage.jsx
+++ src/pages/Profile/ProfilePage.jsx
@@ -2,7 +2,9 @@ import MyView from '../../components/Profile/myView/MyView';
 import YourView from '../../components/Profile/yourView/YourView';
 
 function ProfilePage() {
-  const isMyProfile = true; // true로 바꾸면 MyView가 보입니다.
+  const isMyProfile = false;
+  // true로 바꾸면 MyView
+  // false로 바꾸면 yourView
   return <>{isMyProfile ? <MyView /> : <YourView />}</>;
 }
 export default ProfilePage;
